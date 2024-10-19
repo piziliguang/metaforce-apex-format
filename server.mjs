a279bd3c5840c7011fde15be967d7ff5ac954494
Allen Li <allen.li.salesforce@gmail.com>
2024-10-19 17:14:00 +0800

    updates

--- server.mjs
+++ server.mjs
@@ -6,8 +6,10 @@ const port = 3000;
 
 app.post('/', async (req, res) => {
     let { apexCode, prettierOptions } = req.body || {}, result = {};
+    if (!prettierOptions) prettierOptions = {};
+
     try {
-        let formatted = await prettier.format(apexCode || '', { semi: true, tabWidth: 4, printWidth: 120, ...prettierOptions, parser: prettierOptions.anonymous ? 'apex-anonymous' : 'apex', plugins: [apexPrettierPlugin] })
+        let formatted = await prettier.format(apexCode || '', { semi: true, tabWidth: 4, printWidth: 120, ...prettierOptions, parser: prettierOptions?.anonymous ? 'apex-anonymous' : 'apex', plugins: [apexPrettierPlugin] })
         result = { isSucceeded: true, formatted };
     } catch (ex) {
         result = { isSucceeded: false, formatted: ex.message };
