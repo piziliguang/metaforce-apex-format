393c41cec1639c2d26dbb3dc8e5b6fe90cabdab6
Allen Li <allen.li.salesforce@gmail.com>
2024-10-19 16:04:20 +0800

    updates

--- server.mjs
+++ server.mjs
@@ -4,10 +4,10 @@ import * as apexPrettierPlugin from "prettier-plugin-apex";
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     let result = {};
     try {
-        let formatted = prettier.format('public class Test{ Integer i = 1; }', { semi: true, tabWidth: 4, printWidth: 120, parser: 'apex', plugins: [apexPrettierPlugin] })
+        let formatted = await prettier.format('public class Test{ Integer i = 1; }', { semi: true, tabWidth: 4, printWidth: 120, parser: 'apex', plugins: [apexPrettierPlugin] })
         result = { isSucceeded: true, formatted };
     } catch (ex) {
         result = { isSucceeded: true, formatted: ex.getMessage() };
