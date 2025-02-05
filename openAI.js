a3997f3d2bbf021479a82b4b5512e0c7f11e7c8a
Allen Li <allen.li.salesforce@gmail.com>
2025-02-05 16:21:47 +0800

    updates

--- openAI.js
+++ openAI.js
@@ -11,37 +11,32 @@ const openai = new OpenAI({
 async function requestAI ({ model, messages, isPartial }) {
     let completion = await openai.chat.completions.create({
         model, messages,
-        temperature: 0
+        temperature: 0.2
     });
 
     if (isPartial) {
         return completion.choices[0].message.content;
     } else {
         let newContent = completion.choices[0].message.content;
-        return newContent.split('\n').slice(1, -1).join('\n');
+        if (newContent.includes('<$>')) {
+            return newContent.split('<$>')[1].replace(/^\n|\n$/g, '');
+        } else {
+            return newContent.split('\n').slice(1, -1).join('\n');
+        }
     }
 }
 
 const TongYi = {
-    async completeApex (code) {
-        return await requestAI({
-            model: "qwen-coder-plus",
-            messages: [
-                { "role": "system", "content": `你是一个salesforce apex编程专家，专门优化salesforce apex代码。通过分析输入的Apex代码，在原代码的基础上自动补全缺失的逻辑，代码缩进要根据输入代码的格式进行调整，最后返回补全之后的完整代码。` },
-                { "role": "user", "content": code }
-            ],
-        });
-    },
-
     async optimizeApex (code) {
         return await requestAI({
             model: "qwen-coder-plus",
             messages: [
                 {
                     "role": "system", "content": `你是一个salesforce开发专家，专门优化改进 Salesforce Apex, LWC, Aura 的代码逻辑。请按照以下规则处理输入的代码段：
-1. 检查代码中变量的命名是否有错别字，代码逻辑是否冗余，
-2. 尝试纠正并优化代码逻辑
-4. 只返回优化之后代码，不添加任何解释。` },
+1. 检查代码中变量的命名是否有错别字，代码逻辑是否冗余。
+2. 尝试纠正并优化代码逻辑，不添加任何解释。
+3. 保持代码的原始格式和缩进。
+4. 返回优化之后的代码，并使用<$>包裹。` },
                 { "role": "user", "content": code }
             ]
         });
@@ -58,22 +53,12 @@ const TongYi = {
    - 对于类：生成类的简要描述，列出其主要属性和方法。
    - 对于方法：生成方法的简要描述，列出其参数、返回值和功能说明。
 3. 生成的文档注释应符合Apex语言的规范（使用类似Javadoc的格式）。
-4. 保留代码的原始结构和格式，不要修改代码逻辑。
-5. 只返回代码段，不添加任何解释。` },
+4. 保留代码的原始格式和缩进，不要修改代码逻辑，不添加任何解释。
+5. 返回代码段，并使用<$>包裹。` },
                 { "role": "user", "content": code }
             ]
         });
-    },
-
-    async generateApexTest (code) {
-        return await requestAI({
-            model: "qwen-coder-plus",
-            messages: [
-                { "role": "system", "content": `你是一个salesforce开发专家，专门为 Salesforce Apex, LWC, Aura 代码写测试类。依据 Salesforce Apex 的代码规范，分析输入的代码并写为其写出对应的Apex测试类。注意不要添加任何代码逻辑，不做额外解释。最后返回测试代码。` },
-                { "role": "user", "content": code }
-            ]
-        });
-    },
+    }
 }
 
 export { TongYi }
